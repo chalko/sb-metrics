3a48fe9796e1d3519911053055e132de8815870f
Nick Chalko <nick@chalko.com>
2020-10-25 00:07:48 -0700

    More parsing

--- collector.go
+++ collector.go
@@ -5,6 +5,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"io"
 	"log"
+	"math"
+	"strconv"
+	"strings"
 )
 
 type CableModemCollector struct {
@@ -12,23 +15,21 @@ type CableModemCollector struct {
 
 type ModemStatus struct {
 	startup StartupStatus
-	ds [] DownStatus
+	ds      []DownStatus
 }
 type StartupStatus struct {
 	bootState string
 }
 
 type DownStatus struct {
-
-	id string
-    lock string
-	mod string
-	freg int64
-	power float32
-	snr float32
-	corr int64
-	uncorr int64
-
+	id     string
+	lock   string
+	mod    string
+	freq   int
+	power  float64
+	snr    float64
+	corr   int
+	uncorr int
 }
 
 func scrape(reader io.Reader) ModemStatus {
@@ -37,34 +38,33 @@ func scrape(reader io.Reader) ModemStatus {
 		log.Fatal(err)
 	}
 	tables := doc.Find("table")
-    startup := tables.Filter("table:has(th:contains('Startup Procedure'))")
-    ds := tables.Filter("table:has(th:contains('Downstream Bonded Channels'))")
+	startup := tables.Filter("table:has(th:contains('Startup Procedure'))")
+	ds := tables.Filter("table:has(th:contains('Downstream Bonded Channels'))")
 
 	return ModemStatus{
 		startup: parseStartupStatus(startup),
-		ds: parseDs(ds),
+		ds:      parseDs(ds),
 	}
 }
-
 
 // Procedure Status Comment
 type Psc struct {
 	p string
 	s string
 	c string
-
 }
+
 func parseStartupStatus(table *goquery.Selection) StartupStatus {
-	m := make(map[string]Psc)
-	table.Find("tr:has(td)") .Each(func(i int, s *goquery.Selection) {
-		log.Print(s.Html())
+
+	rows := nonHeaderRows(table)
+	m := make(map[string]Psc, rows.Length())
+	rows.Each(func(i int, s *goquery.Selection) {
 		cells := s.Find("td")
 		psc := Psc{
 			p: cells.Get(0).FirstChild.Data,
 			s: cells.Get(1).FirstChild.Data,
-
 		}
-		m[psc.p]=psc
+		m[psc.p] = psc
 	})
 
 	return StartupStatus{
@@ -73,22 +73,61 @@ func parseStartupStatus(table *goquery.Selection) StartupStatus {
 
 }
 
+func nonHeaderRows(table *goquery.Selection) *goquery.Selection {
+	return table.Find("tr:has(td)").Filter("tr:not(:has(td strong))")
+}
+
 func parseDs(table *goquery.Selection) []DownStatus {
-	rows := table.Find("tr:has(td)").Filter("tr:not(:has(td strong))")
-	ds :=  make([]DownStatus, rows.Length(), 32)
+	rows := nonHeaderRows(table)
+	ds := make([]DownStatus, rows.Length(), 32)
 	rows.Each(func(i int, s *goquery.Selection) {
 		cells := s.Find("td")
 		ds[i] = DownStatus{
-			id: cells.Get(0).FirstChild.Data,
+			id:     text(cells, 0),
+			lock:   text(cells, 1),
+			mod:    text(cells, 2),
+			freq:   freq(text(cells, 3)),
+			power:  float(text(cells, 4)," dBmV"),
+			snr:    float(text(cells, 4)," dB"),
+			corr:   corr(text(cells, 6)),
+			uncorr: corr(text(cells, 7)),
 		}
 	})
-    return ds
+	return ds
 }
 
+func corr(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return -1
+	}
+	return i
+
+}
+
+func float(s string, suffix string) float64 {
+	f, err := strconv.ParseFloat(strings.TrimSuffix(s,suffix), 64)
+	if err != nil {
+		return math.NaN()
+	}
+	return f
+}
+
+func freq(s string) int {
+	i, err := strconv.Atoi(strings.TrimSuffix(s," Hz"))
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
+func text(cells *goquery.Selection, i int) string {
+	node := cells.Get(i)
+	return node.FirstChild.Data
+}
 
 func (cc CableModemCollector) Collect(ch chan<- prometheus.Metric) {
 
-   //ms = scrape("http://192.168.100.1")
+	//ms = scrape("http://192.168.100.1")
 
 }
-
--- collector_test.go
+++ collector_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"testing"
@@ -17,23 +18,14 @@ func TestScrape(t *testing.T) {
 	assert.Equal(t, ModemStatus{
 		startup: StartupStatus{bootState: "OK"},
 		ds: []DownStatus{
-			{id: "16", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "1", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "2", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "3", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "4", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "5", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "6", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "7", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "8", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "9", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "10", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "11", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "12", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "13", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "14", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "15", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
-			{id: "17", lock: "", mod: "", freg: 0, power: 0, snr: 0, corr: 0, uncorr: 0},
+			{id: "16", lock: "Locked", mod: "QAM256", freq: 549000000, power: -4.6, snr: math.NaN(), corr: 204, uncorr: 443},
+			{id: "1", lock: "Locked", mod: "QAM256", freq: 459000000, power: -0.2, snr: math.NaN(), corr: 984, uncorr: 3588},
+			{id: "2", lock: "Locked", mod: "QAM256", freq: 465000000, power: -0.4, snr: math.NaN(), corr: 1050, uncorr: 3613},
+			{id: "3", lock: "Locked", mod: "QAM256", freq: 471000000, power: -0.6, snr: math.NaN(), corr: 867, uncorr: 3231},
+			{id: "4", lock: "Locked", mod: "QAM256", freq: 477000000, power: -0.7, snr: math.NaN(), corr: 885, uncorr: 3630},
+			{id: "5", lock: "Locked", mod: "QAM256", freq: 483000000, power: -1, snr: math.NaN(), corr: 962, uncorr: 3633},
+			{id: "6", lock: "Locked", mod: "QAM256", freq: 489000000, power: -1, snr: math.NaN(), corr: 1014, uncorr: 3502},
+			{id: "7", lock: "Locked", mod: "QAM256", freq: 495000000, power: -1, snr: math.NaN(), corr: 1032, uncorr: 4042},
 		},
 	}, ans)
 
