a8e6cb0251308da101f0b51e8d252ebcc366af77
Nick Chalko <nick@chalko.com>
2020-10-26 21:43:51 -0700

    Add addr flag

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"flag"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 )
 
 var (
-
+	 addr = flag.String("listen-address", ":8888", "The address to listen on for HTTP requests.")
 )
 
 func init() {
@@ -21,9 +22,9 @@ func main() {
 	mux.Handle("/metrics", promhttp.Handler())
 	// The Handler function provides a default handler to expose metrics
 	// via an HTTP server. "/metrics" is the usual endpoint for that.
-	const addr =":8888"
-	log.Printf("starting exporer on %q",addr)
-	if err := http.ListenAndServe(addr,mux); err != nil {
+
+	log.Printf("starting exporter on %q",addr)
+	if err := http.ListenAndServe(*addr,mux); err != nil {
 		log.Fatalf("cannot start exporter: %s",err)
 	}
 }
